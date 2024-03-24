bd84bcbff6d207444b74f0dbb11b012b5df7f8fb
BuildTools <unconfigured@null.spigotmc.org>
2024-03-23 17:48:46 -0700

    add SEO and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
+import robotsTxt from "astro-robots-txt";
 import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
   site: 'https://opentronika.github.io',
-  base: 'lcd-editor-pro'
+  base: 'lcd-editor-pro',
+  integrations: [tailwind(), vue(),robotsTxt()],
 });
